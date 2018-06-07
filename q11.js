966ee1ea6afbb779f34733a7f81a46a43e55f319
sahil chhikara <coolestsahil26@hmail.com>
2018-06-08 00:32:35 +0530

    11th question

---
+++ q11.js
@@ -0,0 +1,7 @@
+var x = 7.65
+var sot = x.toFixed();
+console.log(sot) //Convert the value to a nearest integer and console the result.
+
+//Calculate the square root of above variable and console the result
+var y = x * x;
+console.log(y)
